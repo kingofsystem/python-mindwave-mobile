130e630fa7471b1a106d278921a477232d85be31
Вадим Залюбовский <kingofsystem@ya.ru>
2015-12-11 09:49:07 +0700

    Coding fix

--- example/read_mindwave_mobile.py
+++ example/read_mindwave_mobile.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import time
 import bluetooth
 from mindwavemobile.MindwaveDataPoints import RawDataPoint, AttentionDataPoint
--- mindwavemobile/MindwaveDataPointReader.py
+++ mindwavemobile/MindwaveDataPointReader.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from MindwaveMobileRawReader import MindwaveMobileRawReader
 import struct
 import collections
